cf36432820657da01a163b53b317fa072b852f94
Cameron <132536746+cameronjobson@users.noreply.github.com>
2024-05-14 06:13:46 +0000

    fixed unused code and html error in discarge regimen

--- src/DischargeRegimen.js
+++ src/DischargeRegimen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ExpandablePanel from './ExpandablePanel';
 
 function DischargeRegimen({ birthWeight }) {
-  let fullSupply, partialBreastMilk, noBreastMilk, transitionalFormulaDuration, specialNotes, dietitianFollowUp;
+  let fullSupply, partialBreastMilk, noBreastMilk, transitionalFormulaDuration, specialNotes;
 
   // Determine what to display based on the birth weight
   if (birthWeight < 1250) {
@@ -15,7 +15,6 @@ function DischargeRegimen({ birthWeight }) {
     partialBreastMilk = "Breastfeed/EBM ad lib alternating with Transitional Formula 22 cal";
     noBreastMilk = "Transitional Formula 22 cal";
     specialNotes = "(2)Similac Special Care 30; Premature Enfamil 30; MAX 120mL/day Start with dessert feeds 15mL q3h (better tolerance). Partner w/ parents for home volume/frequency at discharge. AND(4)Neosure 22, Enfacare 22";
-    dietitianFollowUp = "NEST Dietitian Follow Up: 2-3 weeks for SSC30/PE30 dessert feeds or HMF fortification; 4-6 weeks for poor weight gain, feeding issues, suboptimal nutrition regimen, high risk social situations or at provider discretion";
   } else if (birthWeight > 1500 && birthWeight <= 2200) {
     fullSupply = "Option 3: Breastfeed/EBM ad lib plus Preterm Formula 30 cal, 120 ml daily until 3 mo corrected age";
     partialBreastMilk = "Breastfeed/EBM ad lib alternating with Transitional Formula 22 cal";
@@ -40,7 +39,7 @@ function DischargeRegimen({ birthWeight }) {
   return (
     <ExpandablePanel title="Discharge Regimen">
       <div style={{ padding: '10px' }}>
-        <p><strong>Growth goals: Individualized using Fenton app or at least 30-35g/d once > 42 wks</strong></p>
+        <p><strong>Growth goals: Individualized using Fenton app or at least 30-35g/d once &gt; 42 wks</strong></p>
         <p><strong>Breast Milk-Full Supply:</strong> {fullSupply}</p>
         <p><strong>Partial Breast Milk:</strong> {partialBreastMilk}</p>
         <p><strong>No Breast Milk:</strong> {noBreastMilk}</p>
